7d1e85cbb39f7181157d3f4b6669e820531afab0
Robert Wood Jr <rdrwood@gmail.com>
2019-02-16 15:49:12 -0800

    setup functions

---
+++ STL_Algs_Cpp_PA7/MergeTester.cpp
@@ -0,0 +1,20 @@
+
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <list>
+using namespace std;
+
+void mergeLists(list<string>, list<string>);
+
+int main() {
+
+	return 0;
+}
+
+
+void mergeLists(list<string> one, list<string> two) {
+
+}
